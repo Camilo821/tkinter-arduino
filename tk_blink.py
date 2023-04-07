89b4ddb25c7da9ad0b48bacb8ce8b4de85dd6a79
Camilo Arboleda <camiloarboleda821@gmail.com>
2023-04-07 18:07:26 -0500

    menu RGB

--- tk_blink.py
+++ tk_blink.py
@@ -7,14 +7,25 @@ from i2c import LiquidCrystal_I2C
 #Definición de board
 board = pymata4.Pymata4()
 
-
+r = 3
+g = 5
+b = 6
 #Definición de pines y de tk
 board.set_pin_mode_digital_output(8) # Definir pin8 como salida
 board.set_pin_mode_digital_input(7) # Definir pin7 como entrada
+board.set_pin_mode_pwm_output(3)
+board.set_pin_mode_pwm_output(5)
+board.set_pin_mode_pwm_output(6)
 LCD = LiquidCrystal_I2C(0x27, 0, 1, board) # Definición de LCD con I2C
 root = tk.Tk() # Definición de la raíz de tkinter
 root.geometry("700x700") # Tamaño de la ventana
 root.title("Firmata") # Definición del título de la ventana
+
+
+def on_select(event):
+    # Obtener el valor seleccionado
+    selected = combobox.get()
+    # Actualizar la etiqueta con el valor seleccionado
 
 # Función de boton de ON
 def buttonON_click():
@@ -25,7 +36,7 @@ def buttonON_click():
 # funcion de boton de OFF
 def buttonOFF_click():
     board.digital_pin_write(8, 0)
-    print("Botón PFF presionado")
+    print("Botón OFF presionado")
 
 frame1 = tk.Frame(root, width=400, height=100)
 frame1.place(x=150,y=200)
@@ -78,6 +89,42 @@ def send_message():
 
 send_button = ttk.Button(root,text="Enviar",command=send_message)
 send_button.place(x=500,y=400)
+# Crea una variable para almacenar el valor seleccionado
+selected_value = tk.StringVar()
 
-
+# Crea el widget Combobox
+combobox = ttk.Combobox(root, textvariable=selected_value)
+combobox["values"] = ("Red", "Green", "Blue", "Yellow", "White", "Magenta")
+combobox.pack()
+combobox.bind("<<ComboboxSelected>>", on_select)
+def onChangeCombobox():
+    current_value = selected_value.get()
+    match current_value:
+        case "Red":
+            board.pwm_write(r, 255)
+            board.pwm_write(b, 0)
+            board.pwm_write(b, 0)
+        case "Green":
+            board.pwm_write(r, 0)
+            board.pwm_write(g, 255)
+            board.pwm_write(b, 0)
+        case "Blue":
+            board.pwm_write(r, 0)
+            board.pwm_write(g, 0)
+            board.pwm_write(b, 255)
+        case "Yellow":
+            board.pwm_write(r, 170)
+            board.pwm_write(g, 205)
+            board.pwm_write(b, 0)
+        case "White":
+            board.pwm_write(r, 255)
+            board.pwm_write(g, 255)
+            board.pwm_write(b, 255)
+        case "Magenta":
+            board.pwm_write(r, 255)
+            board.pwm_write(g, 0)
+            board.pwm_write(b, 255)
+    #print(current_value)
+    root.after(500, onChangeCombobox)
+root.after(500, onChangeCombobox)
 root.mainloop()
